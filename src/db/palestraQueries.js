17dbe78a380c86a9941c9bb1842198e6c76d39b9
Alex <alex.silva.data@gmail.com>
2024-06-13 22:58:59 -0300

    - Adicionadas queries para buscar, criar, atualizar e deletar palestras. - Adicionadas queries para buscar palestras por palestrante e por evento.

--- src/db/palestraQueries.js
+++ src/db/palestraQueries.js
@@ -1,36 +1,46 @@
-const connection = require('./connection');
+const pool = require('./connection');
 
-const insertPalestra = async (tema, data, horario, palestrante_id, evento_id) => {
-  const [result] = await connection.execute(`
-    INSERT INTO palestra (tema, data, horario, palestrante_id, evento_id)
-    VALUES (?, ?, ?, ?, ?);
-  `, [tema, data, horario, palestrante_id, evento_id]);
+// Obter todas as palestras
+const findAllPalestras = () => pool.execute('SELECT * FROM palestras');
 
-  return result;
-};
+// Obter uma palestra por ID
+const findPalestraById = (id) => pool.execute('SELECT * FROM palestras WHERE id = ?', [id]);
 
-const findAllPalestras = () => connection.execute(`
-  SELECT * FROM palestra;
-`);
+// Criar uma nova palestra
+const createPalestra = (palestra) => pool.execute(
+  'INSERT INTO palestras (tema, data_horario, palestrante_id) VALUES (?, ?, ?)',
+  [palestra.tema, palestra.data_horario, palestra.palestrante_id]
+);
 
-const findPalestraById = (id) => connection.execute(`
-  SELECT * FROM palestra WHERE id = ?
-`, [id]);
+// Atualizar uma palestra existente
+const updatePalestra = (id, palestra) => pool.execute(
+  'UPDATE palestras SET tema = ?, data_horario = ?, palestrante_id = ? WHERE id = ?',
+  [palestra.tema, palestra.data_horario, palestra.palestrante_id, id]
+);
 
-const updatePalestra = (id, palestra) => connection.execute(`
-  UPDATE palestra
-  SET tema = ?, data = ?, horario = ?, palestrante_id = ?, evento_id = ?
-  WHERE id = ?
-`, [palestra.tema, palestra.data, palestra.horario, palestra.palestrante_id, palestra.evento_id, id]);
+// Excluir uma palestra
+const deletePalestra = (id) => pool.execute('DELETE FROM palestras WHERE id = ?', [id]);
 
-const deletePalestra = (id) => connection.execute(`
-  DELETE FROM palestra WHERE id = ?
-`, [id]);
+// Obter palestras por palestrante
+const findPalestrasByPalestrante = (palestranteId) => pool.execute(
+  'SELECT * FROM palestras WHERE palestrante_id = ?',
+  [palestranteId]
+);
+
+// Obter palestras por evento
+const findPalestrasByEvento = (eventoId) => pool.execute(
+  `SELECT p.* FROM palestras p
+  JOIN eventos_palestras ep ON p.id = ep.palestra_id
+  WHERE ep.evento_id = ?`,
+  [eventoId]
+);
 
 module.exports = {
-  insertPalestra,
   findAllPalestras,
   findPalestraById,
+  createPalestra,
   updatePalestra,
-  deletePalestra
+  deletePalestra,
+  findPalestrasByPalestrante,
+  findPalestrasByEvento
 };
