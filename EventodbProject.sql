578fc62115c4911acab6e7cef34627148235137a
Alex <alex.silva.data@gmail.com>
2024-06-13 23:03:36 -0300

    Estrutura do Banco

--- EventodbProject.sql
+++ EventodbProject.sql
@@ -1,28 +1,33 @@
-create database eventodb;
-use evetodb;
+CREATE DATABASE registro_eventos;
 
-CREATE TABLE evento (
+USE registro_eventos;
+
+CREATE TABLE palestrantes (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(255) NOT NULL,
-    data DATE NOT NULL,
-    horario TIME NOT NULL,
-    local ENUM('auditório geral', 'laboratório de informática', 'mini auditório') NOT NULL,
-    CONSTRAINT unique_date_location UNIQUE (data, local)
+    titulo ENUM('Especialista', 'Mestre', 'Doutor', 'Outros') NOT NULL
 );
 
-CREATE TABLE palestra (
+CREATE TABLE palestras (
     id INT AUTO_INCREMENT PRIMARY KEY,
     tema VARCHAR(255) NOT NULL,
-    data DATE NOT NULL,
-    horario TIME NOT NULL,
+    data_horario DATETIME NOT NULL,
     palestrante_id INT,
-    evento_id INT,
-    FOREIGN KEY (palestrante_id) REFERENCES palestrante(id),
-    FOREIGN KEY (evento_id) REFERENCES evento(id)
+    FOREIGN KEY (palestrante_id) REFERENCES palestrantes(id)
 );
 
-CREATE TABLE palestrante (
+CREATE TABLE eventos (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(100) NOT NULL,
-    titulo ENUM('especialista', 'mestre', 'doutor') NOT NULL
+    nome VARCHAR(255) NOT NULL,
+    data_inicio DATE NOT NULL,
+    data_fim DATE NOT NULL,
+    local ENUM('Auditório Geral', 'Laboratório de Informática', 'Mini Auditório', 'Outros') NOT NULL
+);
+
+CREATE TABLE eventos_palestras (
+    evento_id INT,
+    palestra_id INT,
+    PRIMARY KEY (evento_id, palestra_id),
+    FOREIGN KEY (evento_id) REFERENCES eventos(id),
+    FOREIGN KEY (palestra_id) REFERENCES palestras(id)
 );
